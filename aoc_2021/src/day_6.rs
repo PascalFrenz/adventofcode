72159d15f540c31d951a183d163fe0df488714a2
Pascal Frenz <frenz_pascal@outlook.de>
2021-12-06 17:05:08 +0100

    day 6 complete

---
+++ aoc_2021/src/day_6.rs
@@ -0,0 +1,65 @@
+use std::collections::HashMap;
+use std::ops::Add;
+
+pub fn task_a(input: &str) -> u64 {
+    let mut lanternfishs = parse_input(input);
+
+    for _ in 0..80 {
+        lanternfishs = calc_next_gen(lanternfishs);
+    }
+
+    return lanternfishs.iter().fold(0, |sum, (_, &v)| sum + v);
+}
+
+pub fn task_b(input: &str) -> u64 {
+    let mut lanternfishs = parse_input(input);
+
+    for _ in 0..256 {
+        lanternfishs = calc_next_gen(lanternfishs);
+    }
+    return lanternfishs.iter().fold(0, |sum, (_, &v)| sum + v);
+}
+
+fn calc_next_gen(lanternfishs: HashMap<u8, u64>) -> HashMap<u8, u64> {
+    return HashMap::from([
+        (0, *lanternfishs.get(&1).unwrap()),
+        (1, *lanternfishs.get(&2).unwrap()),
+        (2, *lanternfishs.get(&3).unwrap()),
+        (3, *lanternfishs.get(&4).unwrap()),
+        (4, *lanternfishs.get(&5).unwrap()),
+        (5, *lanternfishs.get(&6).unwrap()),
+        (6, lanternfishs.get(&7).unwrap() + lanternfishs.get(&0).unwrap()),
+        (7, *lanternfishs.get(&8).unwrap()),
+        (8, *lanternfishs.get(&0).unwrap()),
+    ]);
+}
+
+fn parse_input(input: &str) -> HashMap<u8, u64> {
+    let init_hashmap: HashMap<u8, u64> = HashMap::from([(0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0), (8, 0)]);
+
+    return input.trim()
+        .split(",")
+        .map(|s| s.parse().unwrap())
+        .fold(init_hashmap, |mut acc, n| {
+            acc.insert(n, acc.get(&n).unwrap().add(1));
+            return acc;
+        });
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_task_a_example() {
+        let input = "3,4,3,1,2";
+        assert_eq!(5934, task_a(input))
+    }
+
+    #[test]
+    fn test_task_b_example() {
+        let input = "3,4,3,1,2";
+        assert_eq!(26984457539, task_b(input))
+    }
+}
--- aoc_2021/src/main.rs
+++ aoc_2021/src/main.rs
@@ -7,13 +7,15 @@ mod day_2;
 mod day_3;
 mod day_4;
 mod day_5;
+mod day_6;
 
 fn main() {
     execute_day(1, day_1::task_a, day_1::task_b);
     execute_day(2, day_2::task_a, day_2::task_b);
     execute_day(3, day_3::task_a, day_3::task_b);
-    execute_day(4, day_4::task_a, day_4::task_b);
+    //execute_day(4, day_4::task_a, day_4::task_b);
     execute_day(5, day_5::task_a, day_5::task_b);
+    execute_day(6, day_6::task_a, day_6::task_b);
 }
 
 fn execute_day<T>(day: usize, a: fn(&str) -> T, b: fn(&str) -> T) where T: Debug {
