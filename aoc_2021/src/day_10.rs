8ff1426b34cbcd33a64be73cec33a053b2153e0a
Pascal Frenz <frenz_pascal@outlook.de>
2021-12-10 09:16:10 +0100

    day 10 - initial commit

---
+++ aoc_2021/src/day_10.rs
@@ -0,0 +1,24 @@
+pub fn task_a(input: &str) {
+
+}
+
+pub fn task_b(input: &str) {
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = "";
+
+    #[test]
+    fn test_a_example() {
+        assert_eq!(0, task_a(input));
+    }
+
+    #[test]
+    fn test_b_example() {
+        assert_eq!(0, task_b(input));
+    }
+}
--- aoc_2021/src/main.rs
+++ aoc_2021/src/main.rs
@@ -12,6 +12,7 @@ mod day_6;
 mod day_7;
 mod day_8;
 mod day_9;
+mod day_10;
 
 fn main() {
     let start = Instant::now();
@@ -24,6 +25,7 @@ fn main() {
     execute_day(7, day_7::task_a, day_7::task_b);
     execute_day(8, day_8::task_a, day_8::task_b);
     execute_day(9, day_9::task_a, day_9::task_b);
+    execute_day(10, day_10::task_a, day_10::task_b);
     let end = Instant::now();
     eprintln!("duration = {:#?}", end.duration_since(start));
 }
