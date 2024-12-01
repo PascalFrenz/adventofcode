88cdf7d0e42fde7bda2109ebea0cc7ed534f1aaf
Pascal Frenz <5677521+PascalFrenz@users.noreply.github.com>
2024-12-01 07:29:51 +0100

    prepare day 2

--- aoc_2024/src/test/java/me/frenz/day01/Day02Test.java
+++ aoc_2024/src/test/java/me/frenz/day01/Day02Test.java
@@ -7,34 +7,28 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class Day01Test {
+class Day02Test {
 
     private static final List<String> EXAMPLE_INPUT = """
-            3   4
-            4   3
-            2   5
-            1   3
-            3   9
-            3   3
             """.lines().toList();
 
-    private Day01 day;
+    private Day02 day;
 
     @BeforeEach
     void setUp() {
-        this.day = new Day01(EXAMPLE_INPUT);
+        this.day = new Day02(EXAMPLE_INPUT);
     }
 
     @Test
     void testExampleA() {
         var result = day.part1();
-        assertEquals(11, result);
+        assertEquals(0, result);
     }
 
     @Test
     void testExampleB() {
         var result = day.part2();
-        assertEquals(31, result);
+        assertEquals(0, result);
     }
 
 
