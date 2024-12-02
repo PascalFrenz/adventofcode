a10bf7c7a165add65163ab9d383568c24e3cfca7
Pascal Frenz <5677521+PascalFrenz@users.noreply.github.com>
2024-12-02 08:12:53 +0100

    solve day 2

--- aoc_2024/src/main/java/me/frenz/day01/Day02.java
+++ aoc_2024/src/main/java/me/frenz/day01/Day02.java
@@ -2,6 +2,8 @@ package me.frenz.day01;
 
 import me.frenz.Day;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Day02 extends Day<Integer, Integer> {
@@ -12,11 +14,47 @@ public class Day02 extends Day<Integer, Integer> {
 
     @Override
     protected Integer part1() {
-        return 0;
+        List<List<Integer>> safeLists = input.stream()
+                .map(s -> Arrays.stream(s.split("\\s+")).mapToInt(Integer::parseInt).boxed().toList())
+                .filter(this::isSafe)
+                .toList();
+        return safeLists.size();
     }
 
     @Override
     protected Integer part2() {
-        return 0;
+        List<List<Integer>> safeLists = new ArrayList<>();
+        for (String s : input) {
+            List<Integer> ints = Arrays.stream(s.split("\\s+")).mapToInt(Integer::parseInt).boxed().toList();
+
+            if (isSafe(ints)) {
+                safeLists.add(ints);
+            } else {
+                for (int i = 0; i < ints.size(); i++) {
+                    List<Integer> modifiedList = new ArrayList<>(ints);
+                    modifiedList.remove(i);
+                    if (isSafe(modifiedList)) {
+                        safeLists.add(ints);
+                        break;
+                    }
+                }
+            }
+        }
+        return safeLists.size();
+    }
+
+    private boolean isSafe(List<Integer> ints) {
+        boolean ascending = true;
+        boolean descending = true;
+        boolean safe = true;
+
+        for (int i = 0; i < ints.size() - 1; i++) {
+            int diff = Math.abs(ints.get(i) - ints.get(i + 1));
+            safe &= diff >= 1 && diff <= 3;
+            ascending &= ints.get(i) < ints.get(i + 1);
+            descending &= ints.get(i) > ints.get(i + 1);
+        }
+
+        return safe && (ascending || descending);
     }
 }
--- aoc_2024/src/test/java/me/frenz/day01/Day02Test.java
+++ aoc_2024/src/test/java/me/frenz/day01/Day02Test.java
@@ -10,6 +10,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class Day02Test {
 
     private static final List<String> EXAMPLE_INPUT = """
+            7 6 4 2 1
+            1 2 7 8 9
+            9 7 6 2 1
+            1 3 2 4 5
+            8 6 4 4 1
+            1 3 6 7 9
             """.lines().toList();
 
     private Day02 day;
@@ -22,13 +28,13 @@ class Day02Test {
     @Test
     void testExampleA() {
         var result = day.part1();
-        assertEquals(0, result);
+        assertEquals(2, result);
     }
 
     @Test
     void testExampleB() {
         var result = day.part2();
-        assertEquals(0, result);
+        assertEquals(4, result);
     }
 
 
