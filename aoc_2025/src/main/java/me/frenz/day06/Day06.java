319c02966c7bf864cbf2907bb4e2ebeedb036eb4
Pascal <5677521+PascalFrenz@users.noreply.github.com>
2025-12-07 13:28:47 +0100

    solve day 6 part 2

--- aoc_2025/src/main/java/me/frenz/day06/Day06.java
+++ aoc_2025/src/main/java/me/frenz/day06/Day06.java
@@ -3,8 +3,10 @@ package me.frenz.day06;
 
 import me.frenz.Day;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.function.BiFunction;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Day06 extends Day<Long, Long> {
 
@@ -31,7 +33,102 @@ public class Day06 extends Day<Long, Long> {
 
     @Override
     protected Long part2() {
-        return 0L;
+        final var operatorString = input.getLast().toCharArray();
+        final var inputWithoutOperators = input.subList(0, input.size() - 1);
+
+        final var operators = extractOperators(operatorString);
+        final var columns = columnizeUsing(inputWithoutOperators, new ArrayList<>(operators.keySet()));
+        final var results = computeResultsByColumn(columns, operators);
+
+        return totalSum(results);
+    }
+
+    private static Long totalSum(HashMap<Integer, Long> results) {
+        return results.values().stream().reduce(0L, Long::sum);
+    }
+
+    private HashMap<Integer, Long> computeResultsByColumn(Map<Integer, List<String>> columns, Map<Integer, BiFunction<Integer, Integer, Long>> operators) {
+        final var results = new HashMap<Integer, Long>();
+        for (var column : columns.entrySet()) {
+            List<Long> pivot = pivot(column.getValue()).stream().mapToLong(Long::parseLong).boxed().toList();
+            final BiFunction<Integer, Integer, Long> operation = operators.get(column.getKey());
+            final Optional<Long> result = pivot.stream().reduce((acc, l) -> operation.apply(Math.toIntExact(acc), Math.toIntExact(l)));
+            if (result.isPresent()) {
+                results.put(column.getKey(), result.get());
+            } else {
+                throw new IllegalArgumentException("should not happen!");
+            }
+        }
+        return results;
+    }
+
+    Map<Integer, List<String>> columnizeUsing(List<String> input, List<Integer> splitPoints) {
+        final var columns = new HashMap<Integer, List<String>>();
+        for (var line : input) {
+            splitPoints.sort(Integer::compareTo); // in case they were not sorted yet
+            for (int i = 0; i < splitPoints.size(); i++) {
+                final int splitAt = splitPoints.get(i);
+                final var toAdd = i < splitPoints.size() - 1
+                        ? line.substring(splitAt, splitPoints.get(i + 1) - 1) // not the end of the list
+                        : line.substring(splitAt); // last element of the list
+
+                columns.compute(splitPoints.get(i), (_, val) -> {
+                    if (val == null) {
+                        return new ArrayList<>(List.of(toAdd));
+                    } else {
+                        val.add(toAdd);
+                        return val;
+                    }
+                });
+            }
+        }
+        return columns;
+    }
+
+    static Map<Integer, BiFunction<Integer, Integer, Long>> extractOperators(char[] operatorString) {
+        final Map<Integer, BiFunction<Integer, Integer, Long>> operators = new HashMap<>();
+        for (int i = 0; i < operatorString.length; i++) {
+            switch (operatorString[i]) {
+                case '+':
+                    operators.put(i, (a, b) -> (long) (a + b));
+                    break;
+                case '*':
+                    operators.put(i, (a, b) -> (long) a * b);
+                    break;
+                case ' ':
+                    // skip spaces
+                    break;
+                default:
+                    throw new IllegalArgumentException("Unknown operator: " + operatorString[i]);
+            }
+        }
+        return operators;
+    }
+
+    List<String> pivot(List<String> input) {
+        if (input.size() == 1 && input.getFirst().length() == 1) {
+            return input;
+        }
+        if (input.stream().allMatch(String::isBlank)) {
+            return input;
+        }
+
+        final var maxOpt = input.stream().map(String::length).max(Integer::compareTo);
+        final var pivoted = IntStream.range(0, maxOpt.get())
+                .mapToObj(_ -> "")
+                .collect(Collectors.toCollection(ArrayList::new));
+
+        for (String line : input) {
+            for (int i = 0; i < line.length(); i++) {
+                if (pivoted.get(i) != null) {
+                    pivoted.set(i, (pivoted.get(i) + line.charAt(i)).trim());
+                } else {
+                    pivoted.add(String.valueOf(line.charAt(i)));
+                }
+            }
+        }
+
+        return pivoted;
     }
 
 }
--- aoc_2025/src/test/java/me/frenz/day06/Day06Test.java
+++ aoc_2025/src/test/java/me/frenz/day06/Day06Test.java
@@ -3,9 +3,14 @@ package me.frenz.day06;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.BiFunction;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertLinesMatch;
 
 class Day06Test {
 
@@ -23,6 +28,32 @@ class Day06Test {
 
     @Test
     void testExampleB() {
-        assertEquals(-1L, new Day06(input).part2());
+        assertEquals(3263827L, new Day06(input).part2());
+    }
+
+    @Test
+    void testExtractOperators() {
+        final var map = Day06.extractOperators(input.getLast().toCharArray());
+        assertEquals(Set.of(0, 4, 8, 12), map.keySet());
+        assertEquals(5, map.get(0).apply(1, 5)); // multiply test
+        assertEquals(1, map.get(4).apply(1, 0)); // add test
+    }
+
+    @Test
+    void testColumnizeUsing() {
+        final Map<Integer, List<String>> columnized = new Day06(List.of()).columnizeUsing(input.subList(0, input.size() - 1), new ArrayList<>(List.of(0, 4, 8, 12)));
+        assertLinesMatch(List.of("123", " 45", "  6"), columnized.get(0));
+        assertLinesMatch(List.of("328", "64 ", "98 "), columnized.get(4));
+        assertLinesMatch(List.of(" 51", "387", "215"), columnized.get(8));
+        assertLinesMatch(List.of("64 ", "23 ", "314"), columnized.get(12));
+    }
+
+    @Test
+    void testPivot() {
+        assertLinesMatch(List.of(""), new Day06(List.of()).pivot(List.of("")));
+        assertLinesMatch(List.of("1"), new Day06(List.of()).pivot(List.of("1")));
+        assertLinesMatch(List.of("1", "2", "3"), new Day06(List.of()).pivot(List.of("123")));
+        assertLinesMatch(List.of("14", "25", "36"), new Day06(List.of()).pivot(List.of("123", "456")));
+        assertLinesMatch(List.of("1", "25", "367"), new Day06(List.of()).pivot(List.of("123", " 56", "  7")));
     }
 }
